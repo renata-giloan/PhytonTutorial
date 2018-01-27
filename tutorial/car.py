b77479502582f5effd5266bbcf50bb707499c60c
RenataGiloan <renata.9129@gmail.com>
2018-01-27 20:00:44 +0200

    first python scripts

---
+++ tutorial/car.py
@@ -0,0 +1,9 @@
+listazoo=['maimuta', 'tigru', 'leu', 'elefant', 3, 5.5, 70]
+listaCars=['VW', 'BMW', 'Hyundai','Volvo']
+listaFruits= ['apple', 'orange', 'banana']
+matrix=[listazoo, listaCars, listaFruits]
+print matrix
+
+l=[1,2,3,4,5,6,7,8,9,10]
+for num in l:
+    print num
